62c1522b84bebf383a2dad144d072cd771450733
Ali Aalam <aliaalam.me@gmail.com>
2024-02-15 14:06:33 +0500

    Update Card

--- src/components/CardMap.jsx
+++ src/components/CardMap.jsx
@@ -4,6 +4,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import diamond from '../assets/diamond.svg'
 import dropdown from '../assets/dropdown.svg'
+import { Link } from 'react-router-dom';
 
 
 
@@ -15,13 +16,14 @@ function Card({ cardData }) {
         <div className="relative w-full rounded-xl overflow-hidden px-1">
             <div className="relative">
                 <img className="rounded-[15px]" src={cardData.image} alt="" />
+
                 <div className="absolute inset-0 flex flex-col items-center justify-start p-5">
                     <div className="flex justify-between w-full">
                         <div>
-                     <img className='w-8 h-8' src={diamond} alt="" />
+                            <img className='w-8 h-8 filter invert' src={diamond} alt="" />
                         </div>
                         <div className='cursor-pointer'>
-                        <img className='w-4 h-4 mt-2' src={dropdown} alt="" />
+                            <img className='w-4 h-4 mt-2' src={dropdown} alt="" />
 
                         </div>
                     </div>
@@ -34,37 +36,46 @@ function Card({ cardData }) {
                             <div className='cursor-pointer'>
                                 <p className='font-medium whitespace-nowrap'>{cardData.profession}</p>
                                 <p className='text-end text-md mt-2'>{cardData.followers}</p>
-                           
+
                             </div>
                         </div>
                         <div className="flex justify-between items-center w-full text-white">
                             <div>
-                            
+
                                 <p className='text-sm  font-medium mt-2'>{cardData.location}</p>
                             </div>
                             <div className='cursor-pointer'>
-                               
+
                                 <div className='flex justify-between  gap-2'>
-                                    <span className='bg-white rounded-full w-8 h-8 flex justify-center items-center'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-black">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                                        </svg>
+                                    <a>
+                                        <span className='bg-white rounded-full w-8 h-8 flex justify-center items-center'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-black">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                                            </svg>
 
-                                    </span>
-                                    <span className='bg-white rounded-full w-8 h-8 flex justify-center items-center'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-black">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                        </svg>
+                                        </span>
 
-                                    </span>
-                                    <span className='bg-white rounded-full w-8 h-8 flex justify-center items-center'>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-black">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                        </svg>
+                                    </a>
+                                    <a >
+                                        <span className='bg-white rounded-full w-8 h-8 flex justify-center items-center'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-black">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                            </svg>
+
+                                        </span>
+
+                                    </a>
+                                    <a>
+                                        <span className='bg-white rounded-full w-8 h-8 flex justify-center items-center'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-black">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                            </svg>
 
 
-                                    </span>
+                                        </span>
+
+                                    </a>
 
                                 </div>
                             </div>
@@ -72,7 +83,7 @@ function Card({ cardData }) {
                     </div>
                 </div>
             </div>
-          
+
         </div>
     );
 }
@@ -81,30 +92,30 @@ function Card({ cardData }) {
 function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "red" }}
-        onClick={onClick}
-      />
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "red" }}
+            onClick={onClick}
+        />
     );
-  }
-  
-  function SamplePrevArrow(props) {
+}
+
+function SamplePrevArrow(props) {
     const { className, style, onClick } = props;
     return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "green" }}
-        onClick={onClick}
-      />
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "green" }}
+            onClick={onClick}
+        />
     );
-  }
+}
 
-  
+
 
 function CardMap() {
 
-    
+
     const settings = {
         dots: true,
         slidesToShow: 4,
@@ -113,7 +124,7 @@ function CardMap() {
         autoplay: false,
         swipeToSlide: true,
         infinite: true,
-        speed:500,
+        speed: 500,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
         responsive: [
@@ -158,7 +169,7 @@ function CardMap() {
         speed: 500, // Adjust speed for smoothness
         ease: 'linear', // Adjust easing function for smoother transitions
     };
-    
+
 
     return (
         <div className="grid grid-cols-1  w-full">
